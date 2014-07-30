80357cce685647fece2f2a74a0f9d83d80bb50b7
Anthony Long <antlong@gmail.com>
2014-07-29 22:05:42 -0400

    Was using strings for counts... no good

--- src/game/statistics.js
+++ src/game/statistics.js
@@ -1,15 +1,15 @@
 function Statistics(key) {
     this.id = key;
-    this.manualDigCount = "0";
-    this.manualGatherCount = "0";
-    this.manualScavengeCount = "0";
-    this.autoDigCount = "0";
-    this.autoScavengeCount = "0";
-    this.autoGatherCount = "0";
-    this.autoRefineCount = "0";
-    this.foundItems = "0";
-    this.autoProduceCount = "0";
-    this.craftedItems = "0";
+    this.manualDigCount = 0;
+    this.manualGatherCount = 0;
+    this.manualScavengeCount = 0;
+    this.autoDigCount = 0;
+    this.autoScavengeCount = 0;
+    this.autoGatherCount = 0;
+    this.autoRefineCount = 0;
+    this.foundItems = 0;
+    this.autoProduceCount = 0;
+    this.craftedItems = 0;
 
     // ---------------------------------------------------------------------------
     // general
@@ -43,28 +43,28 @@ function Statistics(key) {
 
     this.load = function() {
         var storageKey = this._getStorageKey();
-        this.manualDigCount = utils.loadInt(storageKey + 'manualDigCount', "0");
-        this.manualGatherCount = utils.loadInt(storageKey + 'manualGatherCount', "0");
-        this.manualScavengeCount = utils.loadInt(storageKey + 'manualScavengeCount', "0");
-        this.autoDigCount = utils.loadInt(storageKey + 'autoDigCount', "0");
-        this.autoGatherCount = utils.loadInt(storageKey + 'autoGatherCount', "0");
-        this.autoScavengeCount = utils.loadInt(storageKey + 'autoScavengeCount', "0");
-        this.autoRefineCount = utils.loadInt(storageKey + 'autoRefineCount', "0");
-        this.foundItems = utils.loadInt(storageKey + 'foundItems', "0");
+        this.manualDigCount = utils.loadInt(storageKey + 'manualDigCount', 0);
+        this.manualGatherCount = utils.loadInt(storageKey + 'manualGatherCount', 0);
+        this.manualScavengeCount = utils.loadInt(storageKey + 'manualScavengeCount', 0);
+        this.autoDigCount = utils.loadInt(storageKey + 'autoDigCount', 0);
+        this.autoGatherCount = utils.loadInt(storageKey + 'autoGatherCount', 0);
+        this.autoScavengeCount = utils.loadInt(storageKey + 'autoScavengeCount', 0);
+        this.autoRefineCount = utils.loadInt(storageKey + 'autoRefineCount', 0);
+        this.foundItems = utils.loadInt(storageKey + 'foundItems', 0);
         this.autoProduceCount = utils.loadInt(storageKey + 'autoProduceCount', '0');
         this.craftedItems = utils.loadInt(storageKey + 'craftedItems', '0');
     };
 
     this.reset = function(fullReset) {
-        localStorage.setValue(this.manualDigCount, "0");
-        localStorage.setValue(this.manualGatherCount, "0");
-        localStorage.setValue(this.manualScavengeCount, "0");
-        localStorage.setValue(this.autoDigCount, "0");
-        localStorage.setValue(this.autoGatherCount, "0");
-        localStorage.setValue(this.autoScavengeCount, "0");
-        localStorage.setValue(this.autoRefineCount, "0");
-        localStorage.setValue(this.foundItems, "0");
-        localStorage.setValue(this.autoProduceCount, "0");
-        localStorage.setValue(this.craftedItems, "0");
+        localStorage.setValue(this.manualDigCount, 0);
+        localStorage.setValue(this.manualGatherCount, 0);
+        localStorage.setValue(this.manualScavengeCount, 0);
+        localStorage.setValue(this.autoDigCount, 0);
+        localStorage.setValue(this.autoGatherCount, 0);
+        localStorage.setValue(this.autoScavengeCount, 0);
+        localStorage.setValue(this.autoRefineCount, 0);
+        localStorage.setValue(this.foundItems, 0);
+        localStorage.setValue(this.autoProduceCount, 0);
+        localStorage.setValue(this.craftedItems, 0);
     };
 };
