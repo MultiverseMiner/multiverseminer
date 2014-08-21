9a81f28f32d1ab3b89ee48b0701b69e6873654a4
Jesse Morgan <morgajel@gmail.com>
2014-08-21 09:21:11 -0400

    renaming dynamically generated stuff

--- db_create.py
+++ db_create.py
@@ -7,7 +7,7 @@ from app import db
 import os.path
 db.create_all()
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
-    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'DB_REPO')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
