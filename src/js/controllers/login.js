112ac40c2a8892ca93f8e2ad9a554bd3b26c6c80
Belar <belar@belardesign.com>
2014-09-06 22:22:21 +0200

    Small login upgrade

--- src/js/controllers/login.js
+++ src/js/controllers/login.js
@@ -1,23 +1,32 @@
 
 angular.module('MultiverseMiner')
 
-.controller('LoginController', ['$scope', '$http', function($scope, $http) {
+.controller('LoginController', ['$scope', '$http', '$timeout', function($scope, $http) {
 
-	$scope.betaSign = {};
-	var submitAction;
+	$scope.betaSigns = [];
 
-	$scope.update = function(beta){
+	$scope.submitAction = false;
 
-		$scope.betaSign = angular.copy(beta);
-		this.data = $scope.betaSign;
+	$scope.update = function(){
+
+		$scope.betaSigns.push({name:$scope.beta.name, mail:$scope.beta.mail});
+		$scope.name = '';
+		$scope.mail = '';
+
+		this.data = $scope.betaSigns;
 
 		$http({
 			method: 'POST',
-			url: localhost, //don't know it yet :(
+			url: url, //don't know it yet
 			data: data
 		}).success(function(submitAction){
 			if(postedData == "OK"){
-				submitAction = true;
+				$scope.submitAction = true;
+				$scope.betaSigns = [];
+
+				$timeout(function(){
+					$scope.submitAction = false;
+				},3000);
 			}
 		});
 
